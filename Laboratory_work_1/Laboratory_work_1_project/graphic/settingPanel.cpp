3e6481bc38cd57b422cd0ea18b5000f1aeee5279
MatmanBJ <matmanbj@gmail.com>
2022-03-19 21:03:13 +0300

    lab_1 program v0.9

--- Laboratory_work_1/Laboratory_work_1_project/graphic/settingPanel.cpp
+++ Laboratory_work_1/Laboratory_work_1_project/graphic/settingPanel.cpp
@@ -29,44 +29,68 @@ SettingPanel::SettingPanel(QWidget *parent) : QWidget(parent)
     grid->setHorizontalSpacing(1); // vertical interval
     grid->setVerticalSpacing(1); // horizontal interval
 
-    QLabel *titleCircle1 = new QLabel("Circle 1:", this);
+    QLabel *titleCircle1 = new QLabel("Circle", this);
     grid->addWidget(titleCircle1, 0, 0, 1, 4);
 
-    QLabel *titleCircle2 = new QLabel("Circle 2:", this);
+    QLabel *titleCircle2 = new QLabel("Point", this);
     grid->addWidget(titleCircle2, 5, 0, 1, 4);
 
-    QLabel *titleShift = new QLabel("Shift:", this);
-    grid->addWidget(titleShift, 9, 0, 1, 4);
+    //QLabel *titleShift = new QLabel("Shift:", this);
+    //grid->addWidget(titleShift, 9, 0, 1, 4);
 
-    QLabel *Xlabel1 = new QLabel("X:", this);
+    QLabel *Xlabel1 = new QLabel("X-coordianate (X) ", this);
     grid->addWidget(Xlabel1, 1, 0, 1, 1);
-    QLabel *Xlabel2 = new QLabel("X:", this);
+    QLabel *Xlabel2 = new QLabel("X-coordianate (X) ", this);
     grid->addWidget(Xlabel2, 6, 0, 1, 1);
-    QLabel *Xlabel3 = new QLabel("X:", this);
-    grid->addWidget(Xlabel3, 10, 0, 1, 1);
 
-    QLabel *Ylabel1 = new QLabel("Y:", this);
+    //QLabel *Xlabel1 = new QLabel("X:", this);
+    //grid->addWidget(Xlabel1, 1, 0, 1, 1);
+    //QLabel *Xlabel2 = new QLabel("X:", this);
+    //grid->addWidget(Xlabel2, 6, 0, 1, 1);
+    //QLabel *Xlabel3 = new QLabel("X:", this);
+    //grid->addWidget(Xlabel3, 10, 0, 1, 1);
+
+    QLabel *Ylabel1 = new QLabel("Y-coordinate (Y) ", this);
     grid->addWidget(Ylabel1, 2, 0, 1, 1);
-    QLabel *Ylabel2 = new QLabel("Y:", this);
+    QLabel *Ylabel2 = new QLabel("Y-coordinate (Y) ", this);
     grid->addWidget(Ylabel2, 7, 0, 1, 1);
-    QLabel *Ylabel3 = new QLabel("Y:", this);
-    grid->addWidget(Ylabel3, 10, 2, 1, 1);
 
-    QLabel *Rlabel1 = new QLabel("R:", this);
-    grid->addWidget(Rlabel1, 1, 2, 1, 1);
-    QLabel *Rlabel2 = new QLabel("R:", this);
-    grid->addWidget(Rlabel2, 6, 2, 1, 1);
+    //QLabel *Ylabel1 = new QLabel("Y:", this);
+    //grid->addWidget(Ylabel1, 2, 0, 1, 1);
+    //QLabel *Ylabel2 = new QLabel("Y:", this);
+    //grid->addWidget(Ylabel2, 7, 0, 1, 1);
+    //QLabel *Ylabel3 = new QLabel("Y:", this);
+    //grid->addWidget(Ylabel3, 10, 2, 1, 1);
 
-    bX1 = new QLineEdit(this); grid->addWidget(bX1, 1, 1, 1, 1);
-    bY1 = new QLineEdit(this); grid->addWidget(bY1, 2, 1, 1, 1);
-    bR1 = new QLineEdit(this); grid->addWidget(bR1, 1, 3, 1, 1);
-    bX2 = new QLineEdit(this); grid->addWidget(bX2, 6, 1, 1, 1);
-    bY2 = new QLineEdit(this); grid->addWidget(bY2, 7, 1, 1, 1);
-    bR2 = new QLineEdit(this); grid->addWidget(bR2, 6, 3, 1, 1);
-    bShiftX = new QLineEdit(this); grid->addWidget(bShiftX, 10, 1, 1, 1);
-    bShiftY = new QLineEdit(this); grid->addWidget(bShiftY, 10, 3, 1, 1);
+    QLabel *Rlabel1 = new QLabel("Semidiameter (R) ", this);
+    grid->addWidget(Rlabel1, 3, 0, 1, 1);
+    //QLabel *Rlabel1 = new QLabel("R:", this);
+    //grid->addWidget(Rlabel1, 1, 2, 1, 1);
+    //QLabel *Rlabel2 = new QLabel("R:", this);
+    //grid->addWidget(Rlabel2, 6, 2, 1, 1);
 
-    bStart = new QPushButton("Start", this);
+    // fields for input values
+
+    bX1 = new QLineEdit(this);
+    grid->addWidget(bX1, 1, 1, 1, 1);
+    bY1 = new QLineEdit(this);
+    grid->addWidget(bY1, 2, 1, 1, 1);
+    bR1 = new QLineEdit(this);
+    grid->addWidget(bR1, 3, 1, 1, 1);
+    //bR1 = new QLineEdit(this);
+    //grid->addWidget(bR1, 1, 3, 1, 1);
+    bX2 = new QLineEdit(this);
+    grid->addWidget(bX2, 6, 1, 1, 1);
+    bY2 = new QLineEdit(this);
+    grid->addWidget(bY2, 7, 1, 1, 1);
+    //bR2 = new QLineEdit(this);
+    //grid->addWidget(bR2, 6, 3, 1, 1);
+    //bShiftX = new QLineEdit(this);
+    //grid->addWidget(bShiftX, 10, 1, 1, 1);
+    //bShiftY = new QLineEdit(this);
+    //grid->addWidget(bShiftY, 10, 3, 1, 1);
+
+    bStart = new QPushButton("DRAW", this);
     grid->addWidget(bStart, 13, 0, 4, 2);
 
     setLayout(grid);
@@ -99,14 +123,14 @@ void SettingPanel::check_and_start()
         bR1->text().isEmpty() ||
 
         bX2->text().isEmpty() ||
-        bY2->text().isEmpty() ||
-        bR2->text().isEmpty() ||
+        bY2->text().isEmpty()// ||
+        //bR2->text().isEmpty() ||
 
-        bShiftX->text().isEmpty() ||
-        bShiftY->text().isEmpty()
+        //bShiftX->text().isEmpty() ||
+        //bShiftY->text().isEmpty()
     )
     {
-        ifFailed(string("All fields must be filled in"));
+        ifFailed(string("Please, input all the values"));
         return;
     }
 
@@ -122,19 +146,19 @@ void SettingPanel::check_and_start()
     res_ok += !is_ok;
     y02 = bY2->text().toInt(&is_ok);
     res_ok += !is_ok;
-    r2 = bR2->text().toInt(&is_ok);
-    res_ok += !is_ok;
+    //r2 = bR2->text().toInt(&is_ok);
+    //res_ok += !is_ok;
     
     int cb_x, cb_y;
-    cb_x = bShiftX->text().toInt(&is_ok);
-    res_ok += !is_ok;
-    cb_y = bShiftY->text().toInt(&is_ok);
-    res_ok += !is_ok;
+    //cb_x = bShiftX->text().toInt(&is_ok);
+    //res_ok += !is_ok;
+    //cb_y = bShiftY->text().toInt(&is_ok);
+    //res_ok += !is_ok;
     res_ok = !res_ok;
 
     if(res_ok != true)
     {
-        ifFailed(string("There is not int in one of the fields"));
+        ifFailed(string("Please, input the integer values"));
         return;
     }
 
@@ -149,7 +173,7 @@ void SettingPanel::check_and_start()
     d = sqrt(d);
     if(d <= r1 + r2)
     {
-        ifFailed(string("The point shouldn't be inside the circle or be on the circle"));
+        ifFailed(string("Please, input the point coordinates outside from the circle, not inside or on the circle"));
         return;
     }
 
@@ -169,6 +193,7 @@ void SettingPanel::check_and_start()
 
     //solve_nonlinear_system(xo1, r1, xo2, r2, &line11, &line12, &line21, &line22, &line31, &line32, &line41, &line42);
 
+    r2 = 0;
     float r = r1;
     float x_0 = x01; // center of circle
     float y_0 = y01; // center of circle
